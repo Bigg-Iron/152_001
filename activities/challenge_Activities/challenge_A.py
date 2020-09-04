ffb10a1b10bc2dcf0744b0a6dd60bdd042a71175
Renzo <19870186+Bigg-Iron@users.noreply.github.com>
2020-09-04 14:25:19 -0600

    5.2.3

--- activities/challenge_Activities/challenge_A.py
+++ activities/challenge_Activities/challenge_A.py
@@ -6,13 +6,36 @@
 Sample output with input: 17
 18 or less 
 
-"""
 
 user_age = int(input())
 if user_age <= 18:
    print('18 or less')
 else:
    print('Over 18')
+
+"""
+
+
+""" 5.2.3: Basic if-else.
+Write an if-else statement for the following:
+If user_tickets is less than 5, assign num_tickets with 1. Else, assign num_tickets with user_tickets.
+
+Sample output with input: 3
+Value of num_tickets: 1
+
+"""
+
+
+user_tickets = int(input())
+
+''' Your solution goes here '''
+if user_tickets < 5:
+    num_tickets = 1
+else:
+    num_tickets = user_tickets
+
+print('Value of num_tickets:', num_tickets)
+
 
 
 
