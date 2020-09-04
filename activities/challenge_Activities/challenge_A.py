8e9fd10d6cbdb5069ea9b49cdb9b35649fd2cf8d
Renzo <19870186+Bigg-Iron@users.noreply.github.com>
2020-09-04 15:58:43 -0600

    5.4.1: Indentation

--- activities/challenge_Activities/challenge_A.py
+++ activities/challenge_Activities/challenge_A.py
@@ -76,7 +76,6 @@ Sample output for input: 1995
 Probably has seat belts.
 Probably has antilock brakes.
 
-"""
 
 car_year = int(input())
 
@@ -93,7 +92,42 @@ if car_year >= 1990:
 if car_year >= 2000:
     print('Probably has airbags.\n')
     
+"""
 
-    
+
+""" 5.4.1: Indentation: Fix the program.
+
+Retype the below code. Fix the indentation as necessary to make the program work.
+    if 'New York' in temperatures:
+        if temperatures['New York'] > 90:
+        print('The city is melting!')
+        else:
+            print('The temperature in New York is', temperatures['New York'])
+else:
+        print('The temperature in New York is unknown.')
+Sample output with input: 105
+
+The city is melting!
+
+temperatures = {
+    'Seattle': 56.5,
+    'New York': float(input()),
+    'Kansas City': 81.9,
+    'Los Angeles': 76.5
+}
+
+''' Your solution goes here '''
+if 'New York' in temperatures:
+    if temperatures['New York'] > 90:
+        print('The city is melting!')
+    else:
+        print('The temperature in New York is', temperatures['New York'])
+
+else:
+    print('The temperature in New York is unknown.')
+
+"""
+   
+
 
 print('...')
