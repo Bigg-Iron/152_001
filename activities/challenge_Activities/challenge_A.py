00fdd9e37e354e7782ab9d36edba93bdc2bf52d0
Renzo <19870186+Bigg-Iron@users.noreply.github.com>
2020-09-04 12:51:51 -0600

    Changed file structure

---
+++ activities/challenge_Activities/challenge_A.py
@@ -0,0 +1,17 @@
+# Challenge Activities 
+
+"""  5.2.2: Basic if-else expression.
+ Write an expression that will cause the following code to print "18 or less" if the value of user_age is 18 or less. Write only the expression.
+
+Sample output with input: 17
+18 or less 
+
+"""
+
+user_age = int(input())
+if ''' Your solution goes here ''':
+   print('18 or less')
+else:
+   print('Over 18')
+
+   
