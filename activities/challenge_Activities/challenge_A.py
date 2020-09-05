95d4da986355965f9e85e76cfedca485447acdf3
Renzo <19870186+Bigg-Iron@users.noreply.github.com>
2020-09-04 18:45:35 -0600

    5.5.3: If-else expression: Operator chaining

--- activities/challenge_Activities/challenge_A.py
+++ activities/challenge_Activities/challenge_A.py
@@ -136,13 +136,29 @@ Write an expression that will print "Dollar or more" if the value of num_cents i
 
 Sample output with input: 109
 Dollar or more
- """
 
 num_cents = int(input())
 if num_cents >= 100:
     print('Dollar or more')
 else:
     print('not a dollar')
+ """
+ 
+ 
+""" 5.5.3: If-else expression: Operator chaining.
+
+Write an expression that will print "in high school" if the value of user_grade is between 9 and 12 (inclusive).
+
+Sample output with input: 10
+in high school
+
+ """
+
+user_grade = int(input())
+if 9 <= user_grade <= 12:
+    print('in high school')
+else:
+    print('not in high school')
 
 
 
