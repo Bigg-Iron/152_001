6cb1d42dcccfa5603f5ed691a4aea61affff0a69
Renzo <19870186+Bigg-Iron@users.noreply.github.com>
2020-09-04 18:26:17 -0600

    5.5.2: Relational Operators.

--- activities/challenge_Activities/challenge_A.py
+++ activities/challenge_Activities/challenge_A.py
@@ -127,7 +127,23 @@ else:
     print('The temperature in New York is unknown.')
 
 """
-   
+
+
+
+""" 5.5.2: Relational operators.
+
+Write an expression that will print "Dollar or more" if the value of num_cents is at least a dollar (100 cents is a dollar).
+
+Sample output with input: 109
+Dollar or more
+ """
+
+num_cents = int(input())
+if num_cents >= 100:
+    print('Dollar or more')
+else:
+    print('not a dollar')
+
 
 
 print('...')
