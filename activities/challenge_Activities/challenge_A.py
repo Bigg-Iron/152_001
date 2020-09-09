e49878e7c4a67e3aa983b87def331e3e193ae7d9
Renzo <19870186+Bigg-Iron@users.noreply.github.com>
2020-09-08 19:22:34 -0600

    5.9.3: Conditional expression: Conditional assignment

--- activities/challenge_Activities/challenge_A.py
+++ activities/challenge_Activities/challenge_A.py
@@ -241,14 +241,29 @@ Sample output with input: -9
 -9 is negative """
 
 
-user_val = int(input())
+# user_val = int(input())
 
-cond_str = "negative" if user_val < 0 else "non-negative"
+# cond_str = "negative" if user_val < 0 else "non-negative"
 
-print(user_val, 'is', cond_str)
+# print(user_val, 'is', cond_str)
  
  
 
+""" 5.9.3: Conditional expression: Conditional assignment.
+
+Using a conditional expression, write a statement that increments num_users if update_direction is 3, otherwise decrements num_users.
+
+Sample output with inputs: 8 3
+New value is: 9 """
+
+
+
+num_users = int(input())
+update_direction = int(input())
+
+num_users = num_users + 1 if update_direction == 3 else num_users - 1
+
+print('New value is:', num_users)
 
 
 
