6448f94c59470ca0764336e49463753194fe6db5
Renzo <19870186+Bigg-Iron@users.noreply.github.com>
2020-09-13 15:48:33 -0600

    6.3.3: While loop: insect growth

--- activities/challenge_Activities/C6.py
+++ activities/challenge_Activities/C6.py
@@ -31,8 +31,47 @@ Sample output with input: 20
 
 Note: If the submitted code has an infinite loop, the system will stop running the code after a few seconds and report "Program end never reached." The system doesn't print the test case that caused the reported message. '''
 
-user_num = int(input())
-while user_num >= 1:
-    user_num = user_num / 2
-    print(user_num)
+# user_num = int(input())
+# while user_num >= 1:
+#     user_num = user_num / 2
+#     print(user_num)
+
+
+
+''' 6.3.2: Bidding example.
+Write an expression that continues to bid until the user enters 'n'.
+
+Sample output with inputs: 'y' 'y' 'n'
+I'll bid $7!
+Continue bidding? I'll bid $15!
+Continue bidding? I'll bid $23!
+Continue bidding?  '''
+
+# import random
+# random.seed(5)
+
+# keep_going = '-'
+# next_bid = 0
+
+# while keep_going != 'n':
+#    next_bid = next_bid + random.randint(1, 10)
+#    print('I\'ll bid ${}!'.format(next_bid))
+#    print('Continue bidding?', end=' ')
+#    keep_going = input()
+   
+
+''' 6.3.3: While loop: Insect growth.
+Given positive integer num_insects, write a while loop that prints, then doubles, num_insects each iteration. Print values ≤ 100. Follow each number with a space.
+
+Sample output with input: 8
+8 16 32 64 '''
+
+
+num_insects = int(input()) # Must be >= 1
+
+while num_insects > 0 and num_insects <= 100:
+    print(num_insects, end = ' ')
+    num_insects = num_insects * 2
+
+    
 
