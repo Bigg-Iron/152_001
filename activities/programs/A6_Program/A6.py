90fe35882276ade8fb3c2dcaa83d2773750b7674
Renzo <19870186+Bigg-Iron@users.noreply.github.com>
2020-09-14 15:13:08 -0600

    minimum(grades)

--- activities/programs/A6_Program/A6.py
+++ activities/programs/A6_Program/A6.py
@@ -12,18 +12,25 @@ def average(grades):
 def maximum(grades):
     maximum = grades[0]
     x = 0
-    z = 0
     while (x < len(grades)):
         if grades[x] > maximum:
             maximum = grades[x]
         else:
             x += 1
+            
     return(float(maximum))
 
 
 
 def minimum(grades):
-    minimum = min(grades)
+    minimum = grades[0]
+    z = 0
+    while (z < len(grades)):
+        if grades[z] < minimum:
+            minimum = grades[z]
+        else:
+            z += 1
+            
     return(float(minimum))
 
 
