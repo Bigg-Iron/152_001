f8c1e5813c63580187c0a250795c2869f6659f6c
Renzo <19870186+Bigg-Iron@users.noreply.github.com>
2020-09-14 14:35:04 -0600

    Fixed average(grades) func

--- activities/programs/A6_Program/A6.py
+++ activities/programs/A6_Program/A6.py
@@ -1,15 +1,26 @@
-def average(grades):
-    average = sum(grades) / len(grades)
-    return(float(average))
+def average(grades): 
+    total = 0
+    e = 0
+    while(e < len(grades)):
+        total = total + grades[e]
+        e += 1
+    # print(total)
+    average = total / len(grades)
+    return(average)
+    
     
 
 def maximum(grades):
-    maximum = max(grades)
+    maximum = grades[0]
     return(float(maximum))
+
+
 
 def minimum(grades):
     minimum = min(grades)
     return(float(minimum))
+
+
 
 def print_grades(grades):
     print(*grades, sep = "\n")
