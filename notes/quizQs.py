cc39f913079e25f9ef310d6b3e804eab67d5c401
Renzo <19870186+Bigg-Iron@users.noreply.github.com>
2020-09-14 11:08:27 -0600

    Added notes

---
+++ notes/quizQs.py
@@ -0,0 +1,52 @@
+# Questions from quizzes
+
+# def f3(s):
+#     i = 0
+#     r = ''
+#     while i < len(s):
+#         r = s[i] + r
+#         i += 2
+#         return r
+
+# print(f3('wake up'))
+
+# s = 0
+# v = 0.5
+# n = 100
+
+# while n > 1:
+#   s += v
+#   v /= 2
+#   n -= 1
+
+# print(s)
+
+# s = "Rotator"
+# t = ""
+# i = 0
+
+# while i < len(s):
+#   if i % 2 == 0:
+#     t = s[i] + t
+#   i += 1
+
+# print(t)
+
+
+
+# def f1(n):
+#     s = 1
+#     while n > 1:
+#         s *= n
+#         n -= 1
+#         return s
+        
+# print(f1(5))
+
+# def f2(s):
+#     i = 0
+#     while s[i] != ' ':
+#         i += 1
+#         return i
+
+# print(f2('Colo State Uni'), f2('CSU'))
