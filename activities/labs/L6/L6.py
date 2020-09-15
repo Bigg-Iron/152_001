ec49ec5f9cae9c38d73498bc15e5962cc511d615
Renzo <19870186+Bigg-Iron@users.noreply.github.com>
2020-09-15 14:31:17 -0600

    Added Lab 6

---
+++ activities/labs/L6/L6.py
@@ -0,0 +1,20 @@
+def countDown(first, last):
+    pass
+
+def oddNumbers(N):
+    pass
+
+def reverse(s):
+    pass
+
+def countInput():
+    pass
+
+# Write some tests here to see if your code works
+def main():
+    pass
+
+# Do not modify the code below
+if __name__ == '__main__':
+    main()
+    
