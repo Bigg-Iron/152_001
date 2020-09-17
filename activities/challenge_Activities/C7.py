efd9cc5da85c60b443cbddf4f1af249250bfd379
Renzo <19870186+Bigg-Iron@users.noreply.github.com>
2020-09-17 15:30:26 -0600

    7.8.2: Function with branch: Popcorn

--- activities/challenge_Activities/C7.py
+++ activities/challenge_Activities/C7.py
@@ -82,16 +82,134 @@ Sample output with inputs: 2 3
 Sample output with inputs: 'RRGBRYYBGY' 'RRGBBRYBGY'
 User score: 4 '''
 
-user_score = 0
-simon_pattern = input()
-user_pattern  = input()
+# user_score = 0
+# simon_pattern = input()
+# user_pattern  = input()
 
-''' Your solution goes here '''
-for s in simon_pattern:
-    if s == user_pattern[user_score]:
-        user_score+=1
-    else:
-        break
+# ''' Your solution goes here '''
+# for s in simon_pattern:
+#     if s == user_pattern[user_score]:
+#         user_score+=1
+#     else:
+#         break
  
-print(user_score)
+# print(user_score)
+
+
+''' 7.7.1: Loop Else 
+Guess the output '''
+# result = 1
+# n = 3
+# while n > -2:
+#     print(n, end=' ')
+#     result *= 3
+#     n -= 1
+# else:
+#     print('\ {}'.format(result))
+# print('done')
+
+# -----------------------------------------------------
+
+# result = 0
+# n = 2
+# while n > -5:
+#     print(n, end=' ')
+#     result -= 4
+#     if result < -15:
+#         print('*')
+#         break
+#     n -= 1
+# else:
+#     print('/ {}'.format(result))
+# print('done')
+
+# result = 0
+# for n in range(4):
+#     print(n, end=' ')
+#     result += 4
+# else:
+#     print('| {}'.format(result))
+# print('done')
+
+# -----------------------------------------------------
+
+# stop = -15
+# total = 0
+# for number in [7, 3, 7, 6, 2, 6]:
+#     print(number, end=' ')
+#     total -= number
+#     if total < stop:
+#         print('@')
+#         break
+# else:
+#     print('| {}'.format(total))
+# print('done')
+
+
+
+''' 7.8.1: Output of functions with branches/loops. '''
+
+# def print_message(message):
+#     if len(message) > 6:
+#         print('too long')
+#     else:
+#         print(message)
+
+# print_message('Thank you so much!')
+# print_message('Hi!')
+
+
+# -----------------------------------------------------
+
+# def compute(numbers):
+#     result = 1
+#     for num in numbers:
+#         result *= num + 3
+#     return result
+
+# values = [5, 7, 6]
+# computed_value = compute(values)
+# print(computed_value)
+
+# -----------------------------------------------------
+
+# def get_numbers():
+#     user_input = input()
+#     values = []
+#     for token in user_input.split():
+#         values.append(int(token))
+#     return values
+
+# def print_selected_numbers():
+#     numbers = get_numbers()
+#     for number in numbers:
+#         if number <= 2:
+#             print(number)
+
+# print_selected_numbers()
+
+
+# -----------------------------------------------------
+
+''' 
+7.8.2: Function with branch: Popcorn.
+
+Define function print_popcorn_time() with parameter bag_ounces. If bag_ounces is less than 3, print "Too small". If greater than 10, print "Too large". Otherwise, compute and print 6 * bag_ounces followed by "seconds". End with a newline.
+
+Sample output with input: 7
+42 seconds '''
+
+def print_popcorn_time(bag_ounces):
+    if bag_ounces < 3:
+        print("Too small")
+    elif bag_ounces > 10:
+        print("Too large")
+    else:
+        s = (6 * bag_ounces)
+        print('{} seconds'.format(s))
+        
+''' Your solution goes here '''
+
+user_ounces = int(input())
+print_popcorn_time(user_ounces)
 
