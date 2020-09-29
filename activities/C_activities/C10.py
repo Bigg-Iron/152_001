c045b52ca5c8a6141991103d082807da8203a155
Renzo <19870186+Bigg-Iron@users.noreply.github.com>
2020-09-29 17:12:45 -0600

    C10.3.1

--- activities/C_activities/C10.py
+++ activities/C_activities/C10.py
@@ -21,11 +21,25 @@ Sort short_names in reverse alphabetic order.
 Sample output with input: 'Jan Sam Ann Joe Tod'
 ['Tod', 'Sam', 'Joe', 'Jan', 'Ann'] """
 
-user_input = input()
-short_names = user_input.split()
+# user_input = input()
+# short_names = user_input.split()
+
+# ''' Your solution goes here '''
+# short_names.sort()
+# short_names.reverse()
+# print(short_names)
+
+""" 10.3.1: Get user guesses.
+Write a loop to populate the list user_guesses with a number of guesses. The variable num_guesses is the number of guesses the user will have, which is read first as an integer. Read integers one at a time using int(input()).
+
+Sample output with input: '3 9 5 2'
+user_guesses: [9, 5, 2] """
+
+num_guesses = int(input())
+user_guesses = []
 
 ''' Your solution goes here '''
-short_names.sort()
-short_names.reverse()
-print(short_names)
-
+for index in range(num_guesses):
+    user_guesses.append(input())
+    
+print('user_guesses:', user_guesses)
