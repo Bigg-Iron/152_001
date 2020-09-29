81328d5287db412a1385475c514d8126711b137b
Renzo <19870186+Bigg-Iron@users.noreply.github.com>
2020-09-29 14:23:42 -0600

    C10.2.1

--- activities/C_activities/C10.py
+++ activities/C_activities/C10.py
@@ -5,11 +5,27 @@ Sample output with input: 'Gertrude Sam Ann Joseph'
 ['Sam', 'Ann', 'Joe']
  """
  
+# user_input = input()
+# short_names = user_input.split()
+
+# ''' Your solution goes here '''
+# del short_names[0]
+# del short_names[-1]
+# short_names.append('Joe')
+# print(short_names)
+
+
+""" 10.2.1: Reverse sort of list.
+Sort short_names in reverse alphabetic order.
+
+Sample output with input: 'Jan Sam Ann Joe Tod'
+['Tod', 'Sam', 'Joe', 'Jan', 'Ann'] """
+
 user_input = input()
 short_names = user_input.split()
 
 ''' Your solution goes here '''
-del short_names[0]
-del short_names[-1]
-short_names.append('Joe')
+short_names.sort()
+short_names.reverse()
 print(short_names)
+
