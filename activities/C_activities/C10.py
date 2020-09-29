e4b308206d9885a3dd3ab4045f6768a63fa7eaf8
Renzo <19870186+Bigg-Iron@users.noreply.github.com>
2020-09-29 17:20:36 -0600

    Fixed C10.3.1

--- activities/C_activities/C10.py
+++ activities/C_activities/C10.py
@@ -40,6 +40,6 @@ user_guesses = []
 
 ''' Your solution goes here '''
 for index in range(num_guesses):
-    user_guesses.append(input())
+    user_guesses.append(int(input()))
     
 print('user_guesses:', user_guesses)
