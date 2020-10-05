92a72c00277347f952082b3bdb6c837eb9fc859c
Renzo <19870186+Bigg-Iron@users.noreply.github.com>
2020-10-05 14:06:02 -0600

    12.2.1

--- ch12/C12/C12.py
+++ ch12/C12/C12.py
@@ -10,15 +10,46 @@ c
 b
 a """
 
-def backwards_alphabet(curr_letter):
-    if curr_letter == 'a':
-        print(curr_letter)
+# def backwards_alphabet(curr_letter):
+#     if curr_letter == 'a':
+#         print(curr_letter)
+#     else:
+#         print(curr_letter)
+#         prev_letter = chr(ord(curr_letter) - 1)
+#         backwards_alphabet(prev_letter)
+
+# starting_letter = input()
+
+# ''' Your solution goes here '''
+# backwards_alphabet(starting_letter)
+
+
+''' 12.2.1: Recursive function: Writing the base case.
+Add an if branch to complete double_pennies()'s base case.
+
+Sample output with inputs: 1 10
+Number of pennies after 10 days: 1024
+
+
+Note: If the submitted code has an infinite loop, the system will stop running the code after a few seconds, and report "Program end never reached." The system doesn't print the test case that caused the reported message.
+'''
+
+# Returns number of pennies if pennies are doubled num_days times
+def double_pennies(num_pennies, num_days):
+    total_pennies = 0
+
+    ''' Your solution goes here '''
+    if num_days == 0:
+        total_pennies = num_pennies
+
     else:
-        print(curr_letter)
-        prev_letter = chr(ord(curr_letter) - 1)
-        backwards_alphabet(prev_letter)
+        total_pennies = double_pennies((num_pennies * 2), (num_days - 1))
 
-starting_letter = input()
+    return total_pennies
 
-''' Your solution goes here '''
-backwards_alphabet(starting_letter)
+# Program computes pennies if you have 1 penny today,
+# 2 pennies after one day, 4 after two days, and so on
+starting_pennies = int(input())
+user_days = int(input())
+
+print('Number of pennies after', user_days, 'days: ', end="")
