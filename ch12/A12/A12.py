72917a25a409d5040ec9227ed5bcb2d788a6d4b2
Renzo <19870186+Bigg-Iron@users.noreply.github.com>
2020-10-05 11:41:50 -0600

    A12

--- ch12/A12/A12.py
+++ ch12/A12/A12.py
@@ -2,7 +2,14 @@
 
 def palindrome(s):
     """Returns True if the string parameter contains a palindrome, False otherwise."""
-    pass
+    if len(s) < 1: 
+        return True
+    else:
+        if s[0] == s[-1]: 
+            return palindrome(s[1:-1]) 
+        else: 
+            return False
+    
 
 
 def main():
