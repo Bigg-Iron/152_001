e3e701efbfdbd9f0a2e0bae83cfd08f67e5823aa
Renzo <19870186+Bigg-Iron@users.noreply.github.com>
2020-10-06 14:42:57 -0600

    Finished fib(n) and factorial(n) recursive functions

--- ch12/L12/L12.py
+++ ch12/L12/L12.py
@@ -1,25 +1,44 @@
+import math
+
+
+
 ''' Examples:
     SEE WRITEUP ABOVE
 '''
 def factorial(n):
-    return -1
+    if n == 1 or n == 0:
+        fact = 1
+    elif n < 0:
+        fact = 0 
+    else:
+        fact = n * factorial(n-1)
+    return fact
 
 ''' Examples:
     SEE WRITEUP ABOVE
 '''
 def fib(n):
-    return -1
+    if n <= 0:
+        return 0
+    elif n == 1:
+        return 1
+    elif n == 2:
+        return 1
+    else:
+        return fib(n-1) + fib(n-2)
 
 ''' Examples:
     SEE WRITEUP ABOVE
 '''
 def exponent(n):
+
     return -1
 
 def main():
+    print(factorial(-2))
     print(factorial(5))
     print(exponent(4))
-    print(fib(7))
+    print(fib(9))
     
 if __name__ == "__main__":
     main()
